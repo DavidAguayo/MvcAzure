4089bb1fa0337b434df335975c3222ccc8c851a7
DavidAguayo <AlumnoMCSD@T06W02>
2018-03-13 13:51:19 +0100

    Initial commit

---
+++ MvcAzure/Controllers/DepartamentosController.cs
@@ -0,0 +1,26 @@
+﻿using MvcAzure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAzure.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        ModeloDepartamentos modelo;
+
+        public DepartamentosController()
+        {
+            this.modelo = new ModeloDepartamentos();
+        }
+
+        // GET: Departamentos
+        public ActionResult Index()
+        {
+            List<DEPT> lista = modelo.GetDepartamentos();
+            return View(lista);
+        }
+    }
+}
---
+++ MvcAzure/Models/ModeloDepartamentos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcAzure.Models
+{
+    public class ModeloDepartamentos
+    {
+        ContextoDepartamentos contexto;
+
+        public ModeloDepartamentos()
+        {
+            this.contexto = new ContextoDepartamentos();
+        }
+
+        public List<DEPT> GetDepartamentos()
+        {
+            var consulta = from datos in contexto.DEPT
+                           select datos;
+            return consulta.ToList();
+        }
+    }
+}
